08861d0b15c4dc179bdd8c9be4932f0437638995
Karl Southern <karl@theangryangel.co.uk>
2021-03-15 16:24:17 +0000

    auth_pysaml squashed commits

---
+++ auth_pysaml_website/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright (C) 2020 GlodoUK <https://www.glodo.uk/>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Saml2 Authentication Integration with Website",
+    "version": "13.0.1.0.0",
+    "category": "Tools",
+    "author": "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/server-auth",
+    "license": "AGPL-3",
+    "depends": ["website", "auth_pysaml"],
+    "external_dependencies": {"python": []},
+    "demo": [],
+    "data": ["views/auth_saml.xml",],
+    "installable": True,
+    "auto_install": False,
+    "summary": """
+    Allows saml provider buttons to be filtered by website
+    """,
+}
